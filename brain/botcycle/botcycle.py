4e3d01ed9ffa052d1e9b657a2d6d5162fb868561
Martino Mensio <martinomensio@outlook.it>
2017-10-19 23:28:09 +0200

    added support for multi-language

--- brain/botcycle/botcycle.py
+++ brain/botcycle/botcycle.py
@@ -8,6 +8,10 @@ from . import bikes
 from .nlu import wit
 from . import persistence
 from . import personalization
+from . import output_sentences
+
+LANGUAGE = os.environ.get('BOT_LANGUAGE', 'EN')
+print('language is ' + LANGUAGE)
 
 sendMessageFunction = None
 
@@ -25,7 +29,7 @@ def process(msg, sendMessage):
         personalization.add_data_from_login(
             chat_id, msg['facebookId'], msg['token'])
         sendMessageFunction(
-            chat_id, 'Thanks for your contribution! Every person that logs in helps me provide better results to everyone!')
+            chat_id, output_sentences.get(LANGUAGE, 'THANK_LOGGED_IN'))
         return
 
     content_type = 'text' if (msg['text'] != '') else (
@@ -33,7 +37,7 @@ def process(msg, sendMessage):
 
     if persistence.is_first_msg(chat_id):
         sendMessageFunction(
-            chat_id, "Welcome! I am BotCycle and can give you bike sharing informations")
+            chat_id, output_sentences.get(LANGUAGE, 'FIRST_MESSAGE'))
 
     #print(content_type, chat_type, chat_id)
     if content_type == 'text':
@@ -41,7 +45,7 @@ def process(msg, sendMessage):
 
         if msg['text'] == '/start':
             sendMessageFunction(
-                chat_id, "I am BotCycle. Try to ask me something about bike sharing!")
+                chat_id, output_sentences.get(LANGUAGE, 'FIRST_MESSAGE'))
             return
 
         if msg['text'] == '👍':
@@ -55,7 +59,7 @@ def process(msg, sendMessage):
         # TODO this is to test facebook login
         if msg['text'] == 'login':
             sendMessageFunction(
-                chat_id, 'please login with facebook to improve recommendations', 'login')
+                chat_id, output_sentences.get(LANGUAGE, 'ASK_LOGIN') , 'login')
             return
 
         if intent:
@@ -79,22 +83,22 @@ def process(msg, sendMessage):
                 askPosition(chat_id)
 
             elif intent['value'] == 'greeting':
-                response = 'Hi there! How can I help?'
+                response = output_sentences.get(LANGUAGE, 'GREET_BACK')
                 sendMessageFunction(chat_id, response)
 
             elif intent['value'] == 'thank':
-                response = 'You\'re welcome!'
+                response = output_sentences.get(LANGUAGE, 'THANK_BACK')
                 sendMessageFunction(chat_id, response)
 
             elif intent['value'] == 'info':
-                response = 'I am BotCycle, a bot that can give you informations about bike sharing in your city.\nTry to ask me something! ;)'
+                response = output_sentences.get(LANGUAGE, 'PROVIDE_INFO')
                 sendMessageFunction(chat_id, response)
 
             elif intent['value'] == 'city_supported':
                 get_nearest_supported_city(chat_id, entities)
 
             elif intent['value'] == 'booking':
-                response = 'Sorry but you can\'t book bikes'
+                response = output_sentences.get(LANGUAGE, 'CANT_BOOK')
                 sendMessageFunction(chat_id, response)
 
             elif intent['value'] == 'end_discussion':
@@ -102,16 +106,16 @@ def process(msg, sendMessage):
 
             else:
                 sendMessageFunction(
-                    chat_id, "Unexpected intent: " + intent['value'])
+                    chat_id, output_sentences.get(LANGUAGE, 'UNEXPECTED_INTENT').format(intent=intent['value']))
 
         else:
             sendMessageFunction(
-                chat_id, "Your sentence does not have an intent")
+                chat_id, output_sentences.get(LANGUAGE, 'NO_INTENT'))
 
     elif content_type == 'location':
         set_position(chat_id, msg['position'])
     else:
-        sendMessageFunction(chat_id, "why did you send " + content_type + "?")
+        sendMessageFunction(chat_id, output_sentences.get(LANGUAGE, 'UNSUPPORTED_CONTENT_TYPE').format(type=content_type))
 
 
 def set_position_str(chat_id, entities):
@@ -121,7 +125,7 @@ def set_position_str(chat_id, entities):
         set_position(chat_id, location, verbose=False)
         markers = [{'type': 'location', 'latitude': location['latitude'],
                     'longitude': location['longitude']}]
-        sendMessageFunction(chat_id, 'Ok I got your position',
+        sendMessageFunction(chat_id, output_sentences.get(LANGUAGE, 'ACK_POSITION'),
                             msg_type='map', markers=markers)
 
 
@@ -129,28 +133,26 @@ def set_position(chat_id, location, verbose=True):
     global sendMessageFunction
     persistence.save_position(chat_id, location)
     if verbose:
-        response = "Ok I got your position"
+        response = output_sentences.get(LANGUAGE, 'ACK_POSITION')
         sendMessageFunction(chat_id, response)
 
 
 def askPosition(chat_id):
     global sendMessageFunction
-    response = 'Where are you?'
+    response = output_sentences.get(LANGUAGE, 'ASK_POSITION')
     sendMessageFunction(chat_id, response, msg_type='request_location')
 
 
 def provideResult(chat_id, station, search_type, buttons=None):
     global sendMessageFunction
     if not station:
-        response = "Impossible to find informations"
+        response = output_sentences.get(LANGUAGE, 'ERROR_SEARCHING')
 
     elif search_type == 'bikes':
-        response = "You can find " + \
-            str(station.bikes) + " free bikes at station " + station.name
+        response = output_sentences.get(LANGUAGE, 'FREE_BIKES').format(count=station.bikes, station_name=station.name)
 
     elif search_type == 'slots':
-        response = "You can find " + \
-            str(station.free) + " empty slots at station " + station.name
+        response = output_sentences.get(LANGUAGE, 'FREE_SLOTS').format(count=station.bikes, station_name=station.name)
 
     if station:
         markers = [{'type': 'location', 'latitude': station.latitude,
@@ -167,7 +169,7 @@ def search_place(place_name):
         response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?key=' +
                                 os.environ['MAPS_TOKEN'] + '&address=' + place_name).json()
     except Exception as e:
-        print("error in communication with nominatim.openstreetmap.org: " + e)
+        print(output_sentences.get(LANGUAGE, 'GEOCODING_ERROR').format(searched=place_name))
 
     places_found = response['results']
 
@@ -203,7 +205,7 @@ def getLocation(chat_id, entities):
         location_name = location_obj.get('value', None)
         location = search_place(location_name)
         if not location:
-            response = 'I could not find a place named ' + location_name
+            response = output_sentences.get(LANGUAGE, 'GEOCODING_ERROR').format(searched=location_name)
             sendMessageFunction(chat_id, response)
 
     elif user_position:
@@ -229,8 +231,7 @@ def recommend(chat_id, results):
     # TODO on messenger and skype no more than three buttons per card
     buttons = askFeedback()
     buttons.append({'type': 'link', 'title': 'details', 'value': rec['url']})
-    sendMessageFunction(chat_id, 'You could try this interesting place: ' +
-                        rec['name'], msg_type='map', markers=markers, buttons=buttons)
+    sendMessageFunction(chat_id, output_sentences.get(LANGUAGE, 'RECOMMEND_PLACE').format(place_name=rec['name']), msg_type='map', markers=markers, buttons=buttons)
 
 
 def search_bike(chat_id, entities):
@@ -242,6 +243,7 @@ def search_bike(chat_id, entities):
     city, result = bikes.search_nearest(location, 'bikes')
     provideResult(chat_id, result, 'bikes', buttons=askFeedback())
 
+    return
     recommend(chat_id, [result])
 
 def haversine(lon1, lat1, lon2, lat2):
@@ -268,9 +270,9 @@ def get_nearest_supported_city(chat_id, entities):
     result_latlng = search_place(supported_city_name)
     # is the city if distance < 500m
     if (haversine(location['longitude'], location['latitude'], result_latlng['longitude'], result_latlng['latitude']) < 0.5):
-        response = 'Yes, {} is supported'.format(meta['city'])
+        response = output_sentences.get(LANGUAGE, 'SUPPORTED_AFFIRMATIVE').format(city=meta['city'])
     else:
-        response = 'Nearest supported city is ' + meta['city']
+        response = output_sentences.get(LANGUAGE, 'SUPPORTED_NEGATIVE').format(nearest_supported=meta['city'])
     sendMessageFunction(chat_id, response)
 
 
@@ -297,17 +299,17 @@ def plan_trip(chat_id, entities):
     if loc_from_str:
         loc_from = search_place(loc_from_str)
         if not loc_from:
-            response = 'I could not find a place named ' + loc_from_str
+            response = output_sentences.get(LANGUAGE, 'GEOCODING_ERROR').format(searched=loc_from_str)
             sendMessageFunction(chat_id, response)
 
     if loc_to_str:
         loc_to = search_place(loc_to_str)
         if not loc_to:
-            response = 'I could not find a place named ' + loc_to_str
+            response = output_sentences.get(LANGUAGE, 'GEOCODING_ERROR').format(searched=loc_to_str)
             sendMessageFunction(chat_id, response)
 
     if not loc_from and not loc_to:
-        response = "Your trip has no origin and no destination"
+        response = output_sentences.get(LANGUAGE, 'INCOMPLETE_TRIP')
         sendMessageFunction(chat_id, response)
         return
 
@@ -328,8 +330,7 @@ def plan_trip(chat_id, entities):
     city2, result_to = bikes.search_nearest(loc_to, 'slots')
 
     if city1 and city2 and city1 is not city2:
-        response = 'Your trip starts at ' + city1 + ' and ends at ' + city2 + \
-            '. You cannot take a bike from one city and go to another one!'
+        response = output_sentences.get(LANGUAGE, 'INTERCITY_TRIP').format(source=city1, destination=city2)
         sendMessageFunction(chat_id, response)
         return
 
@@ -342,5 +343,5 @@ def plan_trip(chat_id, entities):
 def askFeedback():
     return [{'type': 'text', 'value': '👍'}, {'type': 'text', 'value': '👎'}]
 
-wit_token = os.environ['WIT_TOKEN']
+wit_token = os.environ['WIT_TOKEN_' + LANGUAGE]
 extractor = wit.Extractor(wit_token)
---
+++ brain/botcycle/output_sentences.py
@@ -0,0 +1,52 @@
+
+sentences = {
+    "EN": {
+        "THANK_LOGGED_IN": "Thanks for your contribution! Every person that logs in helps me provide better results to everyone!",
+        "FIRST_MESSAGE": "Welcome! I am BotCycle and can give you bike sharing informations",
+        "ASK_LOGIN": "please login with facebook to improve recommendations",
+        "GREET_BACK": "Hi there! How can I help?",
+        "THANK_BACK": "You\'re welcome!",
+        "PROVIDE_INFO": "I am BotCycle, a bot that can give you informations about bike sharing in your city.\nTry to ask me something! ;)",
+        "CANT_BOOK": "Sorry but you can\'t book bikes",
+        "UNEXPECTED_INTENT": "Unexpected intent: {intent}",
+        "NO_INTENT": "Your sentence does not have an intent",
+        "UNSUPPORTED_CONTENT_TYPE": "why did you send {type}?",
+        "ACK_POSITION": "Ok I got your position",
+        "ASK_POSITION": "Where are you?",
+        "ERROR_SEARCHING": "Impossible to find informations. Something went wrong!",
+        "FREE_BIKES": "You can find {count} free bikes at station {station_name}",
+        "FREE_SLOTS": "You can find {count} free slots at station {station_name}",
+        "GEOCODING_ERROR": "Impossible to find a place named {searched}",
+        "RECOMMEND_PLACE": "You could try this interesting place: {place_name}",
+        "SUPPORTED_AFFIRMATIVE": "Yes, {city} is supported",
+        "SUPPORTED_NEGATIVE": "Nearest supported city is {nearest_city}",
+        "INCOMPLETE_TRIP": "Your trip has no source and no destination",
+        "INTERCITY_TRIP": "Your trip starts at {source} and ends at {destination}. You cannot take a bike from one city and go to another one!"
+    },
+    "IT": {
+        "THANK_LOGGED_IN": "Grazie per il contributo! Ogni persona che effettua il login mi aiuta a fornire risultati migliori a tutti!",
+        "FIRST_MESSAGE": "Benvenuto! Sono BotCycle e posso darti informazioni sul servizio di bike sharing",
+        "ASK_LOGIN": "per favore accedi con facebook per ottenere consigli migliori",
+        "GREET_BACK": "Ciao! Come posso aiutarti?",
+        "THANK_BACK": "Prego!",
+        "PROVIDE_INFO": "Sono BotCycle e posso darti informazioni sul servizio di bike sharing nella tua città.\nProva a chiedermi qualcosa! ;)",
+        "CANT_BOOK": "Mi dispiace ma non posso prenotare le bici",
+        "UNEXPECTED_INTENT": "Intento inaspettato: {intent}",
+        "NO_INTENT": "La tua frase non contiene un intento",
+        "UNSUPPORTED_CONTENT_TYPE": "Perché mi hai mandato {type}?",
+        "ACK_POSITION": "Ok ho la tua posizione",
+        "ASK_POSITION": "Dove sei?",
+        "ERROR_SEARCHING": "Impossibile trovare le informazioni. Qualcosa è andato male!",
+        "FREE_BIKES": "Puoi trovare {count} bici libere alla stazione {station_name}",
+        "FREE_SLOTS": "Puoi trovare {count} parcheggi liberi alla stazione {station_name}",
+        "GEOCODING_ERROR": "Impossibile trovare un posto chiamato {searched}",
+        "RECOMMEND_PLACE": "Potresti provare questo posto interessante: {place_name}",
+        "SUPPORTED_AFFIRMATIVE": "Sì, {city} è supportata",
+        "SUPPORTED_NEGATIVE": "La città supportata più vicina è {nearest_city}",
+        "INCOMPLETE_TRIP": "Il tuo viaggio non ha né un punto di partenza né un punto di arrivo",
+        "INTERCITY_TRIP": "Il tuo viaggio inizia a  {source} e finisce a {destination}. Non puoi prendere una bici in una città e portarla in un'altra!"
+    }
+}
+
+def get(lang, string):
+    return sentences[lang][string]
