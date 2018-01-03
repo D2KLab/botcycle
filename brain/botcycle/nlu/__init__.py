7f73cdd8fbe38a2788bf10c2f218754142b1853e
Martino Mensio <martinomensio@outlook.it>
2018-01-03 09:31:21 +0100

    fix async function call

--- brain/botcycle/nlu/__init__.py
+++ brain/botcycle/nlu/__init__.py
@@ -11,6 +11,13 @@ from .joint.inference import NeuralNetWrapper
 from .. import persistence
 
 
+def apply_async_wrapper(nlu, sentence):
+    """
+    A static wrapper for calling a class method in another process 
+    """
+    return nlu.process(sentence)
+
+
 class Nlu(object):
     """
     Wrapper around both wit.ai and neural network jointSLU.
@@ -34,12 +41,11 @@ class Nlu(object):
         Turns a sentence into intent+entities
         """
         if self.type == 'both':
-            # issue both, in separate threads to wait only max(t1,t2) instead
-            # of t1+t2
-            async_result = self.pool.apply_async(self.wit.process, (sentence))
+            # issue both, in separate threads to wait only max(t1,t2) instead of t1+t2
+            async_result = self.pool.apply_async(apply_async_wrapper, (self.wit, sentence))
             nn_result = self.local.process(sentence)
             wit_result = async_result.get()
-            # return only local processing
+            # return only local processing, wit processing is done to keep the request on wit.ai
             result = nn_result
         else:
             result = self.real.process(sentence)
