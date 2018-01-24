better output in training + gradient clipping fix

nlu/joint/main.py
@@ -10,8 +10,6 @@ from . import data
 from .model import Model
 from . import metrics
 
-# maximum length of sentences
-input_steps = 50
 # embedding size for labels
 embedding_size = 64
 # size of LSTM cells
@@ -34,6 +32,8 @@ def get_model(vocabs, tokenizer, language):
 
 
 def train(mode):
+    # maximum length of sentences
+    input_steps = 50
     print(mode)
     # load the train and dev datasets
     test_data, train_data = data.load_data(DATASET, mode)
@@ -98,9 +98,9 @@ def train(mode):
                 if j == 0:
                     index = random.choice(range(len(batch)))
                     # index = 0
-                    print("Input Sentence        : ", batch[index]['words'])
-                    print("Slot Truth            : ", batch[index]['slots'])
-                    print("Slot Prediction       : ", decoder_prediction[index])
+                    print("Input Sentence        : ", batch[index]['words'][:batch[index]['length']])
+                    print("Slot Truth            : ", batch[index]['slots'][:batch[index]['length']])
+                    print("Slot Prediction       : ", decoder_prediction[index][:batch[index]['length']])
                     print("Intent Truth          : ", batch[index]['intent'])
                     print("Intent Prediction     : ", intent[index])
                 slot_pred_length = list(np.shape(decoder_prediction))[1]

nlu/joint/model.py
@@ -219,8 +219,8 @@ class Model:
         self.grads, self.vars = zip(*optimizer.compute_gradients(self.loss))
         #print("vars for loss function: ", self.vars)
         # Clip gradients to prevent exploding ones
-        gradients, _ = tf.clip_by_global_norm(self.grads, 5)  # clip gradients
-        self.train_op = optimizer.apply_gradients(zip(self.grads, self.vars))
+        self.gradients, _ = tf.clip_by_global_norm(self.grads, 5)  # clip gradients
+        self.train_op = optimizer.apply_gradients(zip(self.gradients, self.vars))
 
 
     def step(self, sess, mode, train_batch):